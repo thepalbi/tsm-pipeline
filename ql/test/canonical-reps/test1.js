e5c90a153efe68b540ca3d0039e0be4f041279b6
Max Schaefer <max-schaefer@github.com>
2021-03-01 11:55:30 +0000

    Rename a few events.

--- ql/test/canonical-reps/test1.js
+++ ql/test/canonical-reps/test1.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
-function foo(x) {
-  x/*event: d*/.bar/*event: e*/(0/*event: a*/);
+function foo(x/*event: x*/) {
+  x.bar/*event: bar*/(0/*event: a*/);
   x.y = 1/*event: b*/;
   x.baz({
     y: 2/*event: c*/
   });
-  fs/*event: f*/.writeFileSync/*event: g*/(3/*event: h*/);
+  fs.writeFileSync/*event: wfs*/(3/*event: d*/);
 }
