e67db19fd0599cd7ab85072aab46c573b143421e
Max Schaefer <max-schaefer@github.com>
2021-03-16 11:08:31 +0000

    Automatically determine CodeQL path in run.sh.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-python run.py \
+
+if ! (codeql version) >/dev/null 2>&1; then
+   echo "CodeQL is not on the path."
+   exit 1
+fi
+
+python3 run.py \
    --query-name NoSql \
    --project-list nosql_projects.txt \
-   --CodeQL-executable /home/wright/codeql-home/codeql/codeql \
+   --CodeQL-executable $(which codeql) \
    --QL-source-code ../ql
