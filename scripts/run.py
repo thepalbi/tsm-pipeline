193633680e6d56d0307b989a3fdccbe1a4b18266
Max Schaefer <max-schaefer@github.com>
2021-04-16 11:45:32 +0100

    Rename a few variables for consistency, and isolate conversion from database name to zip file name.

--- scripts/run.py
+++ scripts/run.py
@@ -33,46 +33,46 @@ def convertQueryName(query_name):
     else:
         sys.exit(f'Unknown query name {query_name}')
 
-def databasePath(database_name, project_dir):
-    return os.path.join(f'{project_dir}', f'{database_name}')
+def databasePath(database_name, databases_dir):
+    return os.path.join(f'{databases_dir}', f'{database_name}.zip')
 
-def databaseExists(database_name, project_dir):
-    return os.path.exists(databasePath(database_name, project_dir))
+def databaseExists(database_name, databases_dir):
+    return os.path.exists(databasePath(database_name, databases_dir))
 
-def fetchDatabaseCommand(database_name, project_dir):
+def fetchDatabaseCommand(database_name, databases_dir):
     # We assume that the database_name is stored as a .zip file on blob store
-    database_path = databasePath(database_name, project_dir)
-    command = "azcopy copy " + f'"https://atmcodeqldata.blob.core.windows.net/atm/javascript-databases/nosql_800_no_evaluation/{database_name}' + "?" + "${ATM_BLOB_STORE_SAS_TOKEN}\"" + f' "{database_path}"' + ' --overwrite=true --check-md5 FailIfDifferent --from-to=BlobLocal --recursive'
+    database_zip = database_name + ".zip"
+    database_path = databasePath(database_name, databases_dir)
+    command = "azcopy copy " + f'"https://atmcodeqldata.blob.core.windows.net/atm/javascript-databases/nosql_800_no_evaluation/{database_zip}' + "?" + "${ATM_BLOB_STORE_SAS_TOKEN}\"" + f' "{database_path}"' + ' --overwrite=true --check-md5 FailIfDifferent --from-to=BlobLocal --recursive'
     return command
 
-def fetchDatabase(database_name, project_dir):
+def fetchDatabase(database_name, databases_dir):
     # Conditionally fetch the database
-    full_database_name = database_name + ".zip"
-    if not databaseExists(full_database_name, project_dir):
+    if not databaseExists(database_name, databases_dir):
         '''
         We need to ensure that the unzip folder name is the same as the zipped folder name.
         We can't guarantee this, so we unzip to a clean temporary directory first before
         copying to unzipped contents to their final location with a correct name
         '''
-        tempUnzipDirectory = os.path.join(project_dir, "tmp")
+        tempUnzipDirectory = os.path.join(databases_dir, "tmp")
         shutil.rmtree(tempUnzipDirectory, ignore_errors=True)
         os.makedirs(tempUnzipDirectory)
         # Fetch from Azure blob store
-        command = fetchDatabaseCommand(full_database_name, project_dir)
+        command = fetchDatabaseCommand(database_name, databases_dir)
         try:
             print(f'-Fetching {database_name}')
             print(f'-using command: {command}')
             subprocess.check_call(command, text=True, shell=True)
-            if not databaseExists(full_database_name, project_dir):
+            if not databaseExists(database_name, databases_dir):
                 print(f'-Failed to fetch {database_name}')
                 print(f'-Failing command is:')
                 print("\t" + command)
                 return False
             print(f'-Unzipping {database_name}')
             # unzip into tmp directory
-            subprocess.check_call(f'unzip {databasePath(full_database_name, project_dir)} -d {tempUnzipDirectory}', text=True, shell=True)
+            subprocess.check_call(f'unzip {databasePath(database_name, databases_dir)} -d {tempUnzipDirectory}', text=True, shell=True)
             # move (and rename) to correct location
-            subprocess.check_call(f'cp -r {tempUnzipDirectory}/* {project_dir}/{database_name}', text=True, shell=True)
+            subprocess.check_call(f'cp -r {tempUnzipDirectory}/* {databases_dir}/{database_name}', text=True, shell=True)
         except subprocess.CalledProcessError as e:
             print(e.output)
             return False
@@ -93,23 +93,23 @@ def runTSM(projectList, codeQLExecutable, QLSourceCode, query_name, clean):
     resultsDirectory = str(Path(__file__).parent.absolute() / "results")
     resultsLogDirectory = str(Path(__file__).parent.absolute() / "results" / "logs")
     logDirectory = str(Path(__file__).parent.absolute() / "logs")
-    projectDirectory = str(Path(__file__).parent.absolute() / "databases")
+    databasesDirectory = str(Path(__file__).parent.absolute() / "databases")
     # Create results and working folders
     if clean:
         print("-Cleaning local state")
         shutil.rmtree(workingDirectory, ignore_errors=True)
         shutil.rmtree(resultsDirectory, ignore_errors=True)
-        shutil.rmtree(projectDirectory, ignore_errors=True)
+        shutil.rmtree(databasesDirectory, ignore_errors=True)
     os.makedirs(workingDirectory, exist_ok = True)
     os.makedirs(resultsDirectory, exist_ok = True)
     os.makedirs(resultsLogDirectory, exist_ok=True)
     shutil.rmtree(logDirectory, ignore_errors=True)
     os.makedirs(logDirectory)
-    os.makedirs(projectDirectory, exist_ok = True)
+    os.makedirs(databasesDirectory, exist_ok = True)
     # Fetch databases
     databases = listDatabases(projectList)
     for database in databases:
-        fetchDatabase(database, projectDirectory)
+        fetchDatabase(database, databasesDirectory)
     # Write config.json file
     config = "{\n"
     config = config + f'"codeQLExecutable": "{codeQLExecutable}",\n'
@@ -125,7 +125,7 @@ def runTSM(projectList, codeQLExecutable, QLSourceCode, query_name, clean):
     # Invoke TSM
     effective_query_name, query_type = convertQueryName(query_name)
     tsm_path = str(Path(__file__).absolute().parent.parent / "code" / "main.py")
-    command = ["python3", tsm_path, "--project-dir", f'{projectDirectory}',
+    command = ["python3", tsm_path, "--project-dir", f'{databasesDirectory}',
         "--results-dir", f'{resultsDirectory}', "--working-dir", f'{workingDirectory}',
         "--query-name", f'{effective_query_name}', "--query-type", f'{query_type}',
         "--solver=CBC", "--project-list", f'{projectList}',
@@ -134,7 +134,7 @@ def runTSM(projectList, codeQLExecutable, QLSourceCode, query_name, clean):
     subprocess.check_call(command, text=True)
     print("-Combining TSM results")
     combine_scores_path = str(Path(__file__).absolute().parent.parent / "code" / "misc" / "combinescores.py")
-    # N.B. --project-dir parameter is misnamed, it's value should be results-dir
+    # N.B. --project-dir parameter is misnamed, its value should be results-dir
     command = ["python3", combine_scores_path, "--project-dir", f'{resultsDirectory}', "--query-name", effective_query_name]
     subprocess.check_call(command, text=True)
     print(f'-Completed: final output is allscores_{effective_query_name}_avg.txt')
