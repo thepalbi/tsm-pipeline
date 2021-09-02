ea1a180c0b326ca4e33f8cdd035de7817de0c6b1
Diego Garbervetsky <diego.garbervetsky@gmail.com>
2021-09-02 22:31:16 +0000

    add paginated version that can handle smaller embeddings

--- similarity-api/embeddings.py
+++ similarity-api/embeddings.py
@@ -17,24 +17,24 @@ if __name__ == "__main__":
 
     embeddingsReader = EmbeddingsReader(predictionsFile, baseFolder, queryType, 50)
 
-    locationStm = Location("g/chaibio/chaipcr", "frontend/javascripts/libs/angular-sanitize.js",
+    locStm = Location("g/chaibio/chaipcr", "frontend/javascripts/libs/angular-sanitize.js",
                 468, 7, 468, 69)
-    locationFunc = Location("g/chaibio/chaipcr", "frontend/javascripts/libs/angular-sanitize.js",
+    locFunc = Location("g/chaibio/chaipcr", "frontend/javascripts/libs/angular-sanitize.js",
                     457, 5, 468, 69)
     repr =  "(parameter 0 (member open *))"
     # selectedSinks = getSimilarSinks(locationStm, locationFunc)
 
-    locStm = Location.fromString("g_magda-io_magda||file:///magda-gateway/src/createAuthPluginRouter.ts:69:9:69:64")
-    locFunc = Location.fromString("g_magda-io_magda||file:///magda-gateway/src/createAuthPluginRouter.ts:62:23:69:64")
-    # repr = request.args.get('repr')
-    repr = "(parameter 0 (member end *))"
+    # locStm = Location.fromString("g_magda-io_magda||file:///magda-gateway/src/createAuthPluginRouter.ts:69:9:69:64")
+    # locFunc = Location.fromString("g_magda-io_magda||file:///magda-gateway/src/createAuthPluginRouter.ts:62:23:69:64")
+    # # repr = request.args.get('repr')
+    # repr = "(parameter 0 (member end *))"
 
-    locStm = Location("g/bkimminich/juice-shop", "routes/chatbot.ts",
-                    62, 3, 64, 3)
-    locFunc = Location("g/bkimminich/juice-shop", "routes/chatbot.ts",
-                    43, 1, 64, 3)
-    repr = "(parameter 0 (member run *))"
+    # locStm = Location("g/bkimminich/juice-shop", "routes/chatbot.ts",
+    #                 62, 3, 64, 3)
+    # locFunc = Location("g/bkimminich/juice-shop", "routes/chatbot.ts",
+    #                 43, 1, 64, 3)
+    # repr = "(parameter 0 (member run *))"
 
 
-    similar = embeddingsReader.getSimilarSinks(locStm, locFunc, repr)
+    similar = embeddingsReader.getSimilarSinksPaginated(locStm, locFunc, repr)
     print(similar)
--- similarity-api/similarity/similarityWithEmb.py
+++ similarity-api/similarity/similarityWithEmb.py
@@ -53,11 +53,33 @@ def chunks(lst, n):
         yield lst[i:i + n]
 
 class EmbeddingsReader: 
-    def __init__(self, predictionsFile, baseFolder, queryType,  chunk_size):
+    def __init__(self, predictionsFile, baseFolder, queryType,  chunk_size, threshold = 0.92):
         self.baseFolder = baseFolder
         self.queryType = queryType
         self.chunk_size = chunk_size
         self.dictPredRepr = readJsonPredictions(predictionsFile)
+        self.threshold = threshold
+    """ Reads the embeddings for a repr. Since the embeddings are organized by chunks 
+        we concatenate the chunks into a single tensor
+        In case of memory issues, we may need to read only a part of the embeddings 
+    """ 
+    def loadKnownSinkEmbForRepPage(self, repr, prefix, page):
+        # print(knownSinksLocStm.keys())
+        # repr = '"'+repr+'"'
+        if repr in self.dictPredRepr.keys(): 
+            allLocs = list(self.dictPredRepr[repr])[page*self.chunk_size:(page+1)*self.chunk_size]
+            print(repr, len(allLocs))
+            # page = 0
+            embsStm = None
+            try:
+                hash = hashlib.md5(repr.encode())
+                filename = os.path.join(self.baseFolder, "embs", self.queryType, prefix+str(page)+"_"+hash.hexdigest()+".pickle" )
+                embsStm = torch.load( filename)
+            except: 
+                print("There was a problem loading embeddings")
+            return embsStm, allLocs
+        print(repr, " not found")
+        return None, None
 
     """ Reads the embeddings for a repr. Since the embeddings are organized by chunks 
         we concatenate the chunks into a single tensor
@@ -88,14 +110,12 @@ class EmbeddingsReader:
         return None, None
 
     # query a candidate (that exists in the repr) against the set of other candidates for the repr 
-    def query_from_candidates(self, locCodes, code_vecs, pos):
+    def query_from_candidates(self, locCodes, code_vecs, emb):
         # calculate
         # test using cosine:  
-        code_vec_np = code_vecs.detach().numpy()
-        similarity = cosine_similarity( [code_vec_np[pos]], code_vec_np)
+        similarity = cosine_similarity( [emb], code_vecs)
         scores = torch.tensor(similarity)
 
-        print("pos in embedding: ", pos)
         print("scores:", scores)
         # print(probs)
         # print(ids)
@@ -106,11 +126,20 @@ class EmbeddingsReader:
     def checkLocation(self, embs, locs, queryLoc):
         print("query:" , queryLoc)
         pos = locs.index(queryLoc)
-        results = self.query_from_candidates(locs, embs, pos)
+        print("pos in embedding: ", pos)
+
+        code_vec_np = embs.detach().numpy()
+        emb = code_vec_np[pos]
+        results = self.query_from_candidates(locs, code_vec_np, emb)
+
         # print(results)
         return results
     
-
+    """ 
+    Given a repr and the enclosing STM + enclosing Function of a candidate sink
+    return the set of candidate sinks (for the same repr) that has similar code
+    Important: Assumes locationStm in embsAllStm and locationFunc in embsAllFunc 
+    """     
     def getSimilarSinks(self, locationStm, locationFunc, repr):
         embsAllStm, allLocs = self.loadKnownSinkEmbForRep(repr,  "emb_")
         embsAllFunc, allLocs = self.loadKnownSinkEmbForRep(repr, "embF_")
@@ -131,7 +160,7 @@ class EmbeddingsReader:
             scoreFunc, locF = resultFunc[i]
             avgScore = (scoreStm+scoreFunc)/2
             # print(allLocs[i], avgScore)
-            if avgScore > 0.92:
+            if avgScore > self.threshold:
                 # print(allLocs[i])
                 selectedLocs.add((allLocs[i][0],avgScore))
 
@@ -139,6 +168,65 @@ class EmbeddingsReader:
         print(selectedLocs)
         return selectedLocs
 
+
+    """ 
+    Given a repr and the enclosing STM + enclosing Function of a candidate sink
+    return the set of candidate sinks (for the same repr) that has similar code 
+    Paginated version to handle memory contrains.
+    Important: Cannot assume locationStm in embsAllStm and locationFunc in embsAllFunc 
+    """
+    def getSimilarSinksPaginated(self, locationStm, locationFunc, repr):
+        sourceLocStm = locationStm
+        sourceLocFunc = locationFunc
+
+        selectedLocs = set()
+        
+        allLocs = list(self.dictPredRepr[repr])
+        allLocsStm =  [locStm for (loc,locStm,locFunc) in allLocs]
+
+        # find and load the embedding for the location 
+        pos = allLocsStm.index(sourceLocStm)
+        page = int(pos / self.chunk_size)
+        relativePos = pos % self.chunk_size
+        # load page with the embedding for that enclosing stm and get embedding
+        embsAllStm, locsStm = self.loadKnownSinkEmbForRepPage(repr,  "emb_", page)
+        code_vec_np = embsAllStm.detach().numpy()
+        embStm = code_vec_np[relativePos]
+        # load page with the embedding for that enclosing func and get embedding
+        embsAllFunc, locsFunc = self.loadKnownSinkEmbForRepPage(repr, "embF_", page)
+        code_vec_np = embsAllFunc.detach().numpy()
+        embFunc = code_vec_np[relativePos]
+
+        print("Negative example: ", sourceLocStm )
+        page = 0
+        for locs in chunks(allLocs, self.chunk_size):
+            print(page)
+            embsAllStm, _ = self.loadKnownSinkEmbForRepPage(repr,  "emb_", page)
+            embsAllFunc, _ = self.loadKnownSinkEmbForRepPage(repr, "embF_", page)
+            locsStm =  [locStm for (loc,locStm,locFunc) in locs]
+            locsFunc =  [locFunc for (loc,locStm,locFunc) in locs]
+
+            code_vec_np = embsAllStm.detach().numpy()
+            resultStm = self.query_from_candidates(locsStm, code_vec_np, embStm)
+            code_vec_np = embsAllFunc.detach().numpy()
+            resultFunc = self.query_from_candidates(locsStm, code_vec_np, embFunc)
+
+            # resultStm = self.checkLocation(embsAllStm, locsStm, sourceLocStm)
+            # resultFunc = self.checkLocation(embsAllFunc, locsFunc, sourceLocFunc)
+
+            for i in range(0,len(locs)):        
+                scoreStm, locS = resultStm[i]
+                scoreFunc, locF = resultFunc[i]
+                avgScore = (scoreStm+scoreFunc)/2
+                # print(allLocs[i], avgScore)
+                if avgScore > self.threshold:
+                    # print(allLocs[i])
+                    selectedLocs.add((locs[i][0],avgScore))
+            page = page + 1
+
+        print("Highlighted ", len(selectedLocs), "/", len(self.dictPredRepr[repr]))
+        print(selectedLocs)
+        return selectedLocs
 
 # baseFolder = "./dbs"
 # testing embeddings for negative examples
