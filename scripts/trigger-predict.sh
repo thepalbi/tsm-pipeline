b0caeb424edc6936a1eec05c5494d3ec57bdea44
Max Schaefer <max-schaefer@github.com>
2021-09-27 13:51:31 +0100

    Check that lists of test/training projects contain no more than 256 projects.

--- scripts/trigger-predict.sh
+++ scripts/trigger-predict.sh
@@ -13,6 +13,16 @@ query="$1"
 training_projects="$2"
 test_projects="$3"
 
+# ensure size limitations: cannot have more than 256 training or test projects
+if [ $(cat "$training_projects" | wc -l) -gt 256 ]; then
+    echo "Too many training projects; maximum 256."
+    exit 1
+fi
+if [ $(cat "$test_projects" | wc -l) -gt 256 ]; then
+    echo "Too many test projects; maximum 256."
+    exit 1
+fi
+
 list_to_json_array() {
     file="$1"
     cat "$file" | awk 'BEGIN { printf "["; sep=" " } { printf "%s\"%s\"", sep, $0; sep=", " } END { print " ]" }'
