c1c1e232c28dda7535a6b7d76265d8106df9352f
Max Schaefer <max-schaefer@github.com>
2021-09-28 11:21:49 +0100

    Allow passing existing directory to triage-locally.sh.

--- scripts/triage-locally.sh
+++ scripts/triage-locally.sh
@@ -3,21 +3,28 @@
 set -e
 
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-WORKFLOW_RUN_ID=$1
+DATA=$1
 CHUNK_SIZE=${2:-1}
 
-if [ -z "$WORKFLOW_RUN_ID" ]; then
-  echo "Usage: $0 <workflow run id> [<chunk size>]"
+if [ -z "$DATA" ]; then
+  echo "Usage: $0 {<workflow run id>|<archive>} [<chunk size>]"
   exit 1
 fi
 
-DEST_DIR=`mktemp -d`
-echo "Downloading workflow artifacts to $DEST_DIR"
-gh run download "$WORKFLOW_RUN_ID" -D "$DEST_DIR" -n _results
+DATA_DIR=`mktemp -d`
+echo "Storing artifacts in $DATA_DIR"
+
+if [ -f "$DATA" ]; then
+    unzip -q -o -d $DATA_DIR $DATA
+else
+    WORKFLOW_RUN_ID=$DATA
+    gh run download "$WORKFLOW_RUN_ID" -D "$DATA_DIR" -n _results
+fi
+
 echo "Copying predictions.json file to the dashboard data directory"
-cp "$DEST_DIR/predictions.json" "$MYDIR/../triager/data/predictions.json"
+cp "$DATA_DIR/predictions.json" "$MYDIR/../triager/data/predictions.json"
 echo "Starting similarity server"
-"$MYDIR/../similarity-api/server.py" --split-predictions --predictions "$DEST_DIR" --embeddings "$DEST_DIR" --chunk-size "$CHUNK_SIZE" &
+"$MYDIR/../similarity-api/server.py" --split-predictions --predictions "$DATA_DIR" --embeddings "$DATA_DIR" --chunk-size "$CHUNK_SIZE" &
 SERVER_PID=$!
 echo "Starting triaging webapp"
 cd "$MYDIR/../triager"
