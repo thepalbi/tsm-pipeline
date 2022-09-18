3c65cbabb06b04b2444d0d3040fa7cb69dc145e9
Pablo Balbi <pablo.l.balbi@gmail.com>
2022-09-18 15:58:31 +0000

    worse run OK

--- code/scripts/evaluate.py
+++ code/scripts/evaluate.py
@@ -1,7 +1,24 @@
 import argparse
-from utils.process import run_process, RunProcessError
-from utils.clis import resolve_codeqlcli_path
+from utils.process import run_process
+from utils.logging import get_stdout_logger
 from os import path
+from clients.cli import CLIClient
+import sys
+from typing import Tuple, List
+from database.cache import DatabasesCache
+
+log = get_stdout_logger("evaluate-cli")
+
+
+def extract_db_info(db: str) -> Tuple[str, str, str]:
+    if db[-1] == "/":
+        db = db[:-1]
+    spllited_db_path = db.split("/")
+    if len(spllited_db_path) < 3:
+        raise Exception("Failed to extract naming from db path: %s" % (db))
+    owner, name, sha = spllited_db_path[-3], spllited_db_path[-2], spllited_db_path[-1]
+    return owner, name, sha
+
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
@@ -12,21 +29,32 @@ if __name__ == "__main__":
         required=True
     )
     args.add_argument(
-        '--db',
+        '--dbs',
         type=str,
         required=True,
-        help='Path to the CodeQL DB being evaluated'
+        help='File with db keys to evaluate'
+    )
+    args.add_argument(
+        '--cache-root',
+        dest='cache_root',
+        type=str,
+        required=True,
+        help='DBs cache root dir'
     )
     args.add_argument(
         '--output',
         type=str,
         required=True,
-        help='BQRS output file'
+        help='Output folder where to saved proccessed results',
     )
     pa = args.parse_args()
 
     root_dir = run_process('git rev-parse --show-toplevel').stdout.rstrip('\n')
-    print("Root dir configured to: %s" % (root_dir))
+    log.info("Root dir configured to: %s" % (root_dir))
+
+    if not path.isdir(pa.output):
+        log.error("--output should be a directory")
+        sys.exit(1)
 
     search_path = ''
     # TODO: The below just works for tainted path
@@ -39,23 +67,33 @@ if __name__ == "__main__":
         query_file = 'tsm-evaluation/tsm-evaluation/src/PathEvaluation.ql'
         cli_version = '2.10.5'
 
-    cli_path = resolve_codeqlcli_path(cli_version)
+    # Configure codeql query run execution with 2 threads, and 6GB ram
+    cli_client = CLIClient(version=cli_version, query_run_args={
+        "--threads": "2",
+        "--ram": "6000",
+    })
     query_file = path.join(root_dir, query_file)
 
-    query_cmd = [
-        '%s query run' % (cli_path),
-        '-d %s' % (pa.db),
-        '--search-path=%s' % (search_path),
-        '--output=%s' % (pa.output),
-        query_file
-    ]
+    cache = DatabasesCache(pa.cache_root, cli_client.version)
+    dbs = []
+    with open(pa.dbs, 'r') as dbs_file:
+        for db in dbs_file.readlines():
+            db = db.rstrip('\n')
+            _, db_dir = cache.get(db)
+            dbs.append(db_dir)
 
-    try:
-        ran_query = run_process(query_cmd)
-        print('''
-        /dev/stdout:\n%s
-        /dev/stderr:\n%s
-        ''' % (ran_query.stdout, ran_query.stderr))
-    except RunProcessError as e:
-        print("Failed to ran query: %s.\n%s\n%s" %
-              (e.cmd, e.stdout, e.stderr))
+    def do_evaluate_db(db: str):
+        """
+        do_evaluate_db evaluates and decodes the query result from a database.
+
+        :param str db: The path to the CodeQL db. Assumes it's stored in a cache folder structure to retrieve it's name.
+        """
+        owner, name, sha = extract_db_info(db)
+        log.info("Evaluating %s - %s" % (owner, name))
+        bqrs_out = path.join(pa.output, "%s_%s_%s.bqrs" % (owner, name, sha))
+        csv_out = path.join(pa.output, "%s_%s_%s.csv" % (owner, name, sha))
+        cli_client.query_run(db, search_path, bqrs_out, query_file)
+        cli_client.bqrs_decode(bqrs_out, "#select", csv_out)
+
+    for db in dbs:
+        do_evaluate_db(db)
