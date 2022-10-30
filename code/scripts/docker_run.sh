b5d1761c4f42790391eeff1d04c9a7c985417f5e
Pablo Balbi <pablo.l.balbi@gmail.com>
2022-10-30 18:06:32 -0300

    some minotr stuff

--- code/scripts/docker_run.sh
+++ code/scripts/docker_run.sh
@@ -11,15 +11,14 @@ RESULTS_DIR=$2
 # Runs the dockerized version of TSM pipeline, mounting the necessary bind volumes
 function tsm-run() {
     docker run \
-        # Configure CodeQL CLIs repository
-        -e "CODEQL_CLIS_ROOT=$CODEQL_CLIS_ROOT" \
-        -v $CLI_DIR:/cli: \
-        -v $QL_LIB_DIR:/ql: \
-        -v $QL_LIB_WORSE_DIR:/worse_lib: \
-        -v $TMP_DIR:/bigtmp: \
-        -v $RESULTS_DIR:/results: \
-        -v $CACHE_DBS_DIR:/dbs \
-        github.com/thepalbi/tsm-main:latest $@
+    -e "CODEQL_CLIS_ROOT=$CODEQL_CLIS_ROOT" \
+    -v $CLI_DIR:/cli: \
+    -v $QL_LIB_DIR:/ql: \
+    -v $QL_LIB_WORSE_DIR:/worse_lib: \
+    -v $TMP_DIR:/bigtmp: \
+    -v $RESULTS_DIR:/results: \
+    -v $CACHE_DBS_DIR:/dbs: \
+    github.com/thepalbi/tsm-main:latest $@
 }
 
 if [[ "$1" == "--help" ]] ; then
@@ -27,7 +26,7 @@ if [[ "$1" == "--help" ]] ; then
     HELP:
     $0 <project list file in CONTAINER volume> <results dir in HOST>
     "
-
+    
     tsm-run --help
     exit 0
 fi
@@ -35,6 +34,12 @@ fi
 if [[ "$1" == "" ]] ; then
     echo "Missing project list file"
     exit 1
+fi
+
+# Create log directory in temp if no present
+if [ ! -d $TMP_DIR/log ]; then
+    echo "Creating log dir at $TMP_DIR/log"
+    mkdir $TMP_DIR/log
 fi
 
 PROJECT_LIST_FILE=$1
@@ -56,12 +61,12 @@ EOF
 # - Use CBC as solver
 # - Run the whole orchestrator for each project individually
 tsm-run --steps generate_entities,generate_model,optimize \
-    --project.cache_dir /dbs \
-    --project-list $PROJECT_LIST_FILE \
-    --query-type Path \
-    --query-name TaintedPathWorse \
-    --solver CBC \
-    run
+--project.cache_dir /dbs \
+--project-list $PROJECT_LIST_FILE \
+--query-type Path \
+--query-name TaintedPathWorse \
+--solver CBC \
+run
 
 # Combine scores generated for all the results above
 python -m misc.combinescores --results-dir $RESULTS_DIR --query-name TaintedPathWorse --out $RESULTS_DIR/averaged-results.txt --raw-out $RESULTS_DIR/averaged-results.csv
