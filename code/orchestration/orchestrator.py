555dd6503b2c7523a7beacab0bdaadba9492ad03
Pablo <pablo.l.balbi@gmail.com>
2023-05-01 13:48:41 -0300

    removing un-used step

--- code/orchestration/orchestrator.py
+++ code/orchestration/orchestrator.py
@@ -31,7 +31,6 @@ class Orchestrator:
         GenerateModelStep,
         CountRepsStep,
         OptimizeStep,
-        GenerateTSMQueryStep
     ]
     possible_steps = [
         GenerateEntitiesStep,
